3ed8eab4b3d34ca9666e19d5cd75816990877720
Павел Завьялов <zavjalov@gmail.com>
2022-08-24 14:46:49 +0300

    Add test actions

--- Rad/Controllers/SprRadController.cs
+++ Rad/Controllers/SprRadController.cs
@@ -1,4 +1,6 @@
-﻿using Rad.Db;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Rad.Db;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
@@ -11,25 +13,88 @@ namespace Rad.Controllers;
 public class SprRadController : Controller
 {
     private readonly IDbContextFactory<RadDbContext> _dbContextFactory;
+    private readonly MapperConfiguration _autoMapperConfiguration;
 
     public SprRadController(IDbContextFactory<RadDbContext> dbContextFactory)
     {
         this._dbContextFactory = dbContextFactory;
+        this._autoMapperConfiguration = new MapperConfiguration(
+            c => c.CreateProjection<SprRad, SprRadListDto>());
+
     }
 
     /// <summary> Список всех данных РАД </summary>
     [HttpGet("List")]
-    public async Task<IActionResult> List(UserContext userContext, DxDataSourceLoadOptions loadOptions)
+    public async Task<JsonResult> List(UserContext userContext, DxDataSourceLoadOptions loadOptions)
     {
         using var dbContext = await this._dbContextFactory.CreateDbContextAsync();
-        var source = dbContext.SprRads.AsNoTracking();
+        var source = dbContext.SprRads.AsNoTracking().ProjectTo<SprRadListDto>(this._autoMapperConfiguration);
         var data = await DataSourceLoader.LoadAsync(source, loadOptions);
+        if (data.data is List<SprRadListDto> rawRad)
+        {
+            data.data = await this.ListUpdateDto(rawRad);
+        }
         return Json(data);
+    }
+
+    private async Task<List<SprRadListDto>> ListUpdateDto(List<SprRadListDto> sprRadList)
+    {
+        await Task.Yield();
+        foreach (var sprRad in sprRadList)
+        {
+            var op = Random.Shared.Next(3);
+            if (op == 2)
+                sprRad.Actions = new[] { 
+                    new ObjectAction() { Operation = "edit" },
+                    new ObjectAction() { Operation = "delete" },
+                    new ObjectAction() { Operation = "print" },
+                };
+            else if (op == 1)
+            {
+                sprRad.Actions = new[] {
+                    new ObjectAction() { Operation = "edit" },
+                };
+            }
+            else
+                sprRad.Actions = new[] {
+                    new ObjectAction() { Operation = "edit" },
+                    new ObjectAction() { Operation = "delete" },
+                };
+        }
+        return sprRadList;
+    }
+
+    /// <summary> Dto для объекта выгружаемого в список Данных РАД </summary>
+    public class SprRadListDto 
+    {
+        /// <summary> ИД </summary>
+        public long Id { get; set; }
+
+        /// <summary> Полное наименование </summary>
+        public string FullName { get; set; } = "";
+
+        /// <summary> ИНН организации </summary>
+        public string Inn { get; set; } = "";
+
+        /// <summary> КПП организации </summary>
+        public string Kpp { get; set; } = "";
+
+        /// <summary> КБК </summary>
+        public string Kbk { get; set; } = "";
+
+        /// <summary> Дата создания </summary>
+        public DateTime OnDate { get; set; }
+
+        /// <summary> Дата закрытия организации (null если ещё активна) </summary>
+        public DateTime? ToDate { get; set; }
+
+        /// <summary> Допустимые действия для пользователя для объекта </summary>
+        public ObjectAction[] Actions { get; set; } = new ObjectAction[0];
     }
 
     /// <summary> Получить полный документ </summary>
     [HttpGet("Entity")]
-    public async Task<IActionResult> Get(long id)
+    public async Task<JsonResult> Get(long id)
     {
         using var dbContext = await this._dbContextFactory.CreateDbContextAsync();
         var rad = await dbContext.SprRads
---
+++ Rad/Domain/ObjectAction.cs
@@ -0,0 +1,18 @@
+﻿namespace Rad.Domain
+{
+    /// <summary> Действие, которое можно совершить над объектом </summary>
+    public class ObjectAction
+    {
+        /// <summary> Имя операции </summary>
+        public string Operation { get; set; }
+
+        /// <summary> Куда пересылать </summary>
+        public string? endpoint { get; set; }
+
+        /// <summary> Каким рисунком отображать </summary>
+        public string? glyph { get; set; }
+
+        /// <summary> Какую подсказку писать </summary>
+        public string? tooltip { get; set; }
+    }
+}
